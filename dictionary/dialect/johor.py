e76ee1baff485311317473b3c7624127fa13ba72
huseinzol05 <husein.zol05@gmail.com>
2022-10-15 12:56:34 +0800

    added johor dict

--- dictionary/dialect/johor.py
+++ dictionary/dialect/johor.py
@@ -1,43 +1,73 @@
-import time
-from selenium import webdriver
-import json
+# https://www.vocabjohor.com/2020/10/kamus-bahasa-johor.html
+# https://prpm.dbp.gov.my/Cari1?keyword=sah%3B&d=15740&
+# normalized by husein
 
-count = 2
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-extensions')
-chrome_options.add_argument('--profile-directory=Default')
-chrome_options.add_argument("--incognito")
-chrome_options.add_argument("--disable-plugins-discovery");
-chrome_options.add_argument("--start-maximized")
-driver = webdriver.Chrome('../chromedriver', chrome_options=chrome_options)
-driver.delete_all_cookies()
-
-driver.get('http://prpm.dbp.gov.my/Cari1.aspx?keyword=%3d&d=15740&#LIHATSINI')
-
-results = []
-
-def get_page():
-    while True:
-
-        try:
-            t = driver.find_element_by_xpath('//*[@id="MainContent_GridView1"]')
-            print(t)
-            return t.get_attribute('innerHTML')
-        except Exception as e:
-            print(e)
-            time.sleep(3)
-
-
-while True:
-    results.append(get_page())
-    p = '//*[@id="MainContent_GridView1"]/tbody/tr[12]/td/table/tbody/tr/td[%d]/a'%(count)
-    print(p)
-    count += 1
-    try:
-        e = driver.find_element_by_xpath(p)
-        driver.execute_script("arguments[0].click();", e)
-    except:
-        break
-
-with open('johor.json', 'w') as fopen:
-    json.dump(results, fopen)
+dictionary = {
+    'air tawo': ['air tawar'],
+    'bak': ['bawa'],
+    'bandor': ['bandar'],
+    'besor': ['besar'],
+    'bobos': ['bocor'],
+    'bubuh': ['letak'],
+    'cador': ['cadar'],
+    'calor': ['calar'],
+    'cenonet': ['kecil'],
+    'engko': ['awak'],
+    'engkau': ['engkau'],
+    'esbok': ['peti sejuk'],
+    'bersaing': ['bersama-sama'],
+    'sajak': ['sesuai'],
+    'sangap': ['menguap'],
+    'bengap': ['bodoh'],
+    'ngelat': ['tipu'],
+    'selenge': ['skema'],
+    'jotos': ['sekel'],
+    'epok-epok': ['karipap'],
+    'air jarang': ['air kosong', 'air masak'],
+    'gebor': ['selimut'],
+    'sogame': ['sekolah agama'],
+    "pe'el": ['buat perangai'],
+    'jalan tegak': ['jalan lurus'],
+    'sekali harung': ['semua sekali'],
+    'kang': ['nanti'],
+    'godak': ['kacau air'],
+    'gambo': ['gambar'],
+    'grobok': ['almari'],
+    'hangen': ['angin'],
+    'hantor': ['hantar'],
+    'hanto': ['hantar'],
+    'jajan': ['makanan ringan'],
+    'jerang': ['masak nasi', 'masak air'],
+    'korang': ['kamu semua'],
+    'kosi': ['kerusi'],
+    'pak kal': ['nasib lah', 'nasiblah'],
+    'semangka': ['tembikai'],
+    'kopek': ['buka'],
+    'nengok': ['tengok'],
+    'oter': ['ubah'],
+    'kememeh': ['suka menangis', 'kuat menangis'],
+    'sarap': ['sarapan'],
+    'tak semenggah': ['tak senonoh'],
+    'dedor': ['dendam'],
+    'longgo': ['longgar'],
+    'sokkabor': ['surat khabar'],
+    'kecur': ['terliur'],
+    'terejal': ['nakal'],
+    'bigor': ['gila', 'sewel'],
+    'nglupo': ['menggelupur'],
+    'miong': ['pening'],
+    'pijor': ['panas'],
+    'semejid': ['masjid'],
+    'selipo': ['selipar'],
+    'nonong': ['tak peduli'],
+    'terpeleot': ['terseliuh'],
+    'beposah': ['makan'],
+    'sesah': ['makan'],
+    'semonyeh': ['makan berterabur'],
+    'gembek': ['gila'],
+    'putut': ['pendek', 'tumpul'],
+    'menais': ['menenes', 'menyenyeh'],
+    'lolok': ['longkang'],
+    'belocah': ['main air', 'main lopak'],
+    'bobak': ['temberang', 'menipu'],
+}
