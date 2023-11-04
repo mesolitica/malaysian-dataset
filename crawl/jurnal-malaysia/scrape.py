bd2569d496b02ebf1b72406556237bed13b877ae
haizadtarik <haizadtarik@gmail.com>
2023-11-04 18:24:50 +0800

    add scraping script for journal-malaysia

---
+++ crawl/jurnal-malaysia/scrape.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+import logging
+import json
+from tqdm import tqdm
+import time
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Define the base URL of the site to scrape
+base_url = 'https://jurnal-malaysia.com/'
+
+def get_articles(url):
+    time.sleep(5)
+    posts = []
+    links = []
+    titles = []
+    # Get total number of pages from headers by sending a HEAD request to the URL
+    response = requests.head("https://jurnal-malaysia.com/wp-json/wp/v2/posts")
+    total_pages = response.headers.get('X-WP-TotalPages', 'Unknown')
+    for page in tqdm(range(1, int(total_pages) + 1)):
+        logging.debug(f"Retrieve page no {page}")
+        response = requests.get(f'{url}/wp-json/wp/v2/posts?page={page}&per_page=100')
+        if response.ok:
+            links = links + [d['link'] for d in response.json() if 'link' in d]
+            titles = titles + [d['title']['rendered'] for d in response.json() if 'title' in d]
+            posts = posts + [BeautifulSoup(d['content']['rendered'] , 'html.parser').get_text(strip=True) for d in response.json() if ('content' in d and 'rendered' in d['content'])]
+            logging.debug(f"{len(posts)} articles retrieve after querying page {page}")
+
+        else:
+            logging.error(f"Failed to retrieve the page no: {page}. Status code: {response.status_code}")
+    return posts, links, titles
+
+def combine_data(posts, links, titles):
+    combined_list = [{'url': link, 'title': title, 'body': post}
+                 for link, title, post in zip(links, titles, posts)]
+    return combined_list
+
+def save_json(posts):
+    with open('jurnal-malaysia.jsonl', 'a') as f:
+        json.dump(posts, f)
+
+articles, urls, titles = get_articles(base_url)
+data = combine_data(articles, urls, titles)
+save_json(data)
+logging.info(f"Retrieved {len(data)} articles from {base_url}")
