d62732aceedd5995355c68c8a9f05035ef584fa6
Syahir Halim <sangasuragaming@gmail.com>
2024-08-04 18:16:23 +0800

    ikm.org.my-publications

---
+++ crawl/ikm.org.my-publications/myai_ikm_website/myai_ikm_website/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MyaiIkmWebsiteItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
