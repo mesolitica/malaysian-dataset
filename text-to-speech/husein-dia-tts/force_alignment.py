4bf6ae4b0a88ceb9b01b3c6d73932b791866624b
huseinzol05 <husein.zol05@gmail.com>
2025-08-06 12:57:18 +0800

    fix force alignment

--- text-to-speech/husein-dia-tts/force_alignment.py
+++ text-to-speech/husein-dia-tts/force_alignment.py
@@ -2,13 +2,11 @@ import os
 import soundfile as sf
 import json
 import click
-import re
+import numpy as np
 import pandas as pd
-from glob import glob
 from functools import partial
 from multiprocess import Pool
 from tqdm import tqdm
-import time
 
 def chunks(l, devices):
     chunk_size = len(l) // len(devices)
@@ -35,19 +33,21 @@ def loop(
 
     import torchaudio
     from ctc_forced_aligner import (
-        load_audio,
         load_alignment_model,
         generate_emissions,
         preprocess_text,
         get_alignments,
-        get_spans,
-        postprocess_results,
     )
     device = 'cuda'
     alignment_model, alignment_tokenizer = load_alignment_model(
         device,
         dtype=torch.float16 if device == "cuda" else torch.float32,
     )
+    tokenizer = alignment_tokenizer
+    dictionary = tokenizer.get_vocab()
+    dictionary = {k: v for k, v in dictionary.items()}
+    dictionary_rev = {v: k for k, v in dictionary.items()}
+    dictionary["<star>"] = len(dictionary)
 
     df = pd.read_parquet(file)
 
@@ -62,6 +62,7 @@ def loop(
             pass
 
         row = df.iloc[i]
+
         gen_text = row['normalized_generate_text']
         y, sr = sf.read(row['filename_audio'])
         new_wav = torch.from_numpy(y)
@@ -76,15 +77,58 @@ def loop(
             romanize=True,
             language=language,
         )
-        segments, scores, blank_token = get_alignments(
-            emissions,
-            tokens_starred,
-            alignment_tokenizer,
-        )
-        spans = get_spans(tokens_starred, segments, blank_token)
-        word_timestamps = postprocess_results(text_starred, spans, stride, scores)
+        tokens_starred.append('<star>')
+        text_starred.append('<star>')
+        tokens = tokens_starred
+        token_indices = [
+            dictionary[c] for c in " ".join(tokens).split(" ") if c in dictionary
+        ]
+        alignments, scores = torchaudio.functional.forced_align(emissions[None].cpu(), torch.tensor([token_indices]))
+        alignments, scores = alignments[0], scores[0]
+        token_spans = torchaudio.functional.merge_tokens(alignments, scores)
+        groups = []
+        current_group = []
+
+        for span in token_spans:
+            current_group.append(span)
+            if span.token == 31:
+                groups.append(current_group)
+                current_group = []
+
+        if len(current_group):
+            groups.append(current_group)
+
+        merged = []
+        for g in groups:
+            temp, score = [], []
+            if not isinstance(g, list):
+                g = [g]
+            if len(g) == 1 and g[0].token == 31:
+                continue
+            for g_ in g:
+                if g_.token == 31:
+                    continue
+                score.append(g_.score)
+                temp.append(dictionary_rev[g_.token])
+            if len(temp):
+                merged.append({
+                    'text': ''.join(temp),
+                    'start': g[0].start,
+                    'end': g[-1].start + (g[-1].end - g[-1].start) // 2,
+                    'score': np.mean(score)
+                })
+
+        text_nonstar = [w for w in text_starred if w != '<star>']
+        alignment = []
+        for i in range(len(merged)):
+            alignment.append({
+                'text': text_nonstar[i],
+                'start': round((merged[i]['start'] * len(y) / emissions.shape[0]) / sr, 3),
+                'end': round((merged[i]['end'] * len(y) / emissions.shape[0]) / sr, 3),
+                'score': round(float(merged[i]['score']), 3)
+            })
         with open(filename, 'w') as fopen:
-            json.dump(word_timestamps, fopen)
+            json.dump(alignment, fopen)
 
 @click.command()
 @click.option('--file')
@@ -133,7 +177,7 @@ def main(
     )
 
     with Pool(len(devices)) as pool:
-        pooled = pool.map(loop_partial, df_split)
+        pool.map(loop_partial, df_split)
 
 if __name__ == '__main__':
     main()
